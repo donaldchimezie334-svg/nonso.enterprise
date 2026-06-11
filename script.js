2fc7cdebcf82561d2fbb9f657de0c6e6af173a3f
Donald <donald@gmail.com>
2026-06-11 15:10:19 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function scrollToProducts() {
+    document.getElementById("products")
+    .scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+// Fade-in animation
+window.addEventListener("scroll", () => {
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+        const cardTop = card.getBoundingClientRect().top;
+
+        if(cardTop < window.innerHeight - 100){
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+        }
+    });
+});
+
+document.querySelectorAll(".card").forEach(card => {
+    card.style.opacity = "0";
+    card.style.transform = "translateY(40px)";
+    card.style.transition = "0.6s";
+});
